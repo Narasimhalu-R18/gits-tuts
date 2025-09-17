9fb4f57c5718fafb9839b7776d055e57f44f526d
Narasimhalu-R18 <nikilnarasimhalu@gmail.com>
2025-09-17 23:16:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-print("Hello nikil")
+pipeline{
+agent any
+stages
+{
+  stage('BUild')
+  {
+  steps{
+  echo "Building the project"
+}
+}
+stage('Test')
+  {
+  steps{
+  echo "Testing the project"
+}
+}
+stage('Deploy')
+  {
+  steps{
+  echo "Deploying the project"
+}
+}
+}
+}
 
